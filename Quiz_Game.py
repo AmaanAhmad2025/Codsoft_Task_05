cb25371d4a4f5e5a254e851f3bc60ba33f4e2e52
Amaan Ahmad <95515255+AmaanAhmad2025@users.noreply.github.com>
2023-09-23 07:57:30 +0530

    Add files via upload

---
+++ Quiz_Game.py
@@ -0,0 +1,249 @@
+def welcomeMessage():
+    a = '''************* Welcome to this CODESOFT Quiz Game *************
+
+    Rules of the Quiz are as follows:
+    1)- There are total 15 multiple choice questions.
+    2)- For each correct answer you'll be rewarded 4 marks.
+    3)- There is no negative marking.
+    4)- Maximum marks is 60.
+
+********************** ALL THE BEST :) ***********************'''
+    print(a)
+
+def quiz():
+    c=0
+    welcomeMessage()
+    f = input("Press any key to continue")
+    print()
+    Q1= '''Q1) On which of the following rivers does 'Bhakra Nangal Dam' is situated?
+a) Sutluj
+b) Mahi
+c) Ravi
+d) Chenab'''
+    print(Q1)
+    n= input("Enter option: ")
+    if n == "a" or n == "A":
+        print("Nice! your answer is correct")
+        c=c+4
+    else:
+        print("Oops! you're wrong. Correct answer is option A")
+    k = input()
+    print()
+    Q2= '''Q2) In which of the follwing states does 'Khanderi Cricket Stadium' is situated?
+a) Tamil Nadu
+b) Gujrat
+c) Jharkhand
+d) Maharashtra'''
+    print(Q2)
+    n= input("Enter option: ")
+    if n == "b" or n == "B":
+        print("Nice! your answer is correct")
+        c=c+4
+    else:
+        print("Oops! you're wrong. Correct answer is option B")
+    k = input()
+    print()
+    Q3= '''Q3) Which of the following tribal groups lives in Andaman Island?
+a) Jawaras
+b) Aptanis
+c) Urali
+d) Chenchus'''
+    print(Q3)
+    n= input("Enter option: ")
+    if n == "a" or n == "A":
+        print("Nice! your answer is correct")
+        c=c+4
+    else:
+        print("Oops! you're wrong. Correct answer is option A")
+    k = input()
+    print()
+    Q4= '''Q4) Which of the following mosques is the world's largest mosque?
+a) Blue mosque
+b) Hagia Sofia
+c) Masjid al-haram
+d) Jama masjid'''
+    print(Q4)
+    n= input("Enter option: ")
+    if n == "c" or n == "C":
+        print("Nice! your answer is correct")
+        c=c+4
+    else:
+        print("Oops! you're wrong. Correct answer is option C")
+    k = input()
+    print()
+    Q5= '''Q5) In which of the following cities does world's largest library exists?
+a) Baltimoe
+b) Istanbul
+c) Tokyo
+d) Washington DC'''
+    print(Q5)
+    n= input("Enter option: ")
+    if n == "d" or n == "D":
+        print("Nice! your answer is correct")
+        c=c+4
+    else: 
+        print("Oops! you're wrong. Correct answer is option D")
+    k = input()
+    print()
+    Q6='''Q6)Which is the only vowel not used as the first letter in the US State?
+a) A
+b) E
+c) U
+d) I'''
+    print(Q6)
+    n= input("Enter option:")
+    if n == "a" or n == "A":
+        print("Nice! your answer is correct")
+        c=c+4
+    else:
+        print("Oops! you're wrong. Correct answer is option A")
+    k = input()
+    print()
+    Q7='''Q7) Which is the hottest continent?
+a) Asia
+b) Africa
+c) Europe
+d) North America'''
+    print(Q7)
+    n= input("Enter option:")
+    if n == "b" or n == "B":
+        print("Nice! your answer is correct")
+        c=c+4
+    else:
+        print("Oops! you're wrong. Correct answer is option B")
+    k = input()
+    print()
+    Q8='''Q8) Which of the following countries still have a shilling as currency?
+a) Kenya
+b) Uganda
+c) Somalia
+d) All of these'''
+    print(Q8)
+    n= input("Enter option:")
+    if n == "d" or n == "D":
+        print("Nice! your answer is correct")
+        c=c+4
+    else:
+        print("Oops! you're wrong. Correct answer is option D")
+    k = input()
+    print()
+    Q9='''Q9) How many world titles does Phil Talyaor won in darts?
+a) 12
+b) 13
+c) 15
+d) 16'''
+    print(Q9)
+    n= input("Enter option:")
+    if n == "d" or n == "D":
+        print("Nice! your answer is correct")
+        c=c+4
+    else:
+        print("Oops! you're wrong. Correct answer is option D")
+    k = input()
+    print()
+    Q10='''Q10) How many Menmbers of Parliyament(MPs) are there in Lok Sabha?
+a) 200
+b) 300
+c) 550
+d) 650'''
+    print(Q10)
+    n= input("Enter option:")
+    if n == "c" or n == "C":
+        print("Nice! your answer is correct")
+        c=c+4
+    else:
+        print("Oops! you're wrong. Correct answer is option C")
+    k = input()
+    print()
+    Q11='''Q11) Who is Donald Trump's vice president?
+a) Mike Pence
+b) John Adams
+c) Dick Cheney
+d) Joe Biden'''
+    print(Q11)
+    n= input("Enter option:")
+    if n == "a" or n == "A":
+        print("Nice! your answer is correct")
+        c=c+4
+    else:
+        print("Oops! you're wrong. Correct answer is option A")
+    k = input()
+    print()
+    Q12='''Q12) Which of the following is world's longest river?
+a) River Nile
+b) Yangtz
+c) Amazon River
+d) Ganges'''
+    print(Q12)
+    n= input("Enter option:")
+    if n == "a" or n == "A":
+        print("Nice! your answer is correct")
+        c=c+4
+    else:
+        print("Oops! you're wrong. Correct answer is option A")
+    k = input()
+    print()
+    Q13='''Q13) Which is world's largest country?
+a) Russia
+b) Canada
+c) Australia
+d) India'''
+    print(Q13)
+    n= input("Enter option:")
+    if n == "a" or n == "A":
+        print("Nice! your answer is correct")
+        c=c+4
+    else:
+        print("Oops! you're wrong. Correct answer is option A")
+    k = input()
+    print()
+    Q14='''Q14) Which is smallest country in the world?
+a) REPUBLIC OF SAN MARINO
+b) Vatican City
+c) REPUBLIC OF NAURU
+d) TUVALU'''
+    print(Q14)
+    n= input("Enter option:")
+    if n == "b" or n == "B":
+        print("Nice! your answer is correct")
+        c=c+4
+    else:
+        print("Oops! you're wrong. Correct answer is option B")
+    k = input()
+    print()
+    Q15='''Q15) Which vitamin the only one that you will not find in an egg?
+a) Vitamin B
+b) Vitamin C
+c) Vitamin D
+d) Vitamin K'''
+    print(Q15)
+    n= input("Enter option:")
+    if n == "b" or n == "B":
+        print("Nice! your answer is correct")
+        c=c+4
+    else:
+        print("Oops! you're wrong. Correct answer is option B")
+    k = input()
+    print()
+    print("Your final score is",c,"/60")
+    print("Percentage: ", (c/60)*100)
+    o = input()
+    print()
+    while True:
+        j = ''' Do you want to play again?
+        1)- Yes (Press y)
+        2)- No (Press n)'''
+        print(j)
+        v = input()
+        if v not in "yYnN":
+            print("Wrong input")
+        else:
+            return v
+
+while True:
+    
+    t = quiz()
+    if t == "y" or t == "Y":
+        quiz()
+    else:
+        break 
